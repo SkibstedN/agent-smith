67e2a2d6e7d5e714b568493365d475dbfaa6dbbc
Rasmus Buchholdt <rabu@trifork.com>
2023-04-27 11:32:37 +0200

    Init CLI

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import argparse
+
 def init():
     # Init LLM
     pass
 
 
+# https://towardsdatascience.com/how-to-write-user-friendly-command-line-interfaces-in-python-cc3a6444af8e
+
 def main():
-    print("This maybe works?")
+    parser = argparse.ArgumentParser(description="Target a IP ")
+    parser.add_argument("-t", "--target", type=str, help="URL of the target", required=True)
+    parser.add_argument("-p", "--port", type=int, help="Optional port of the target")
+    parser.add_argument("-i", "--info", type=str, help="Optional known information about the target")
+    # parser.add_argument("-b", "--birth", type=str, help="Your birthday in YYYY-MM-DD format", required=True)
+    # parser.add_argument("-m", "--manufacturer", type=str, nargs="+", help="The vaccine manufacturer", required=True, choices=[
+    #         "pfizer","moderna","astrazeneca","janssen","sinovac"])
+    # parser.add_argument("-d", "--date", type=str, nargs="+", help="The date of vaccination", required=True)
+    args = parser.parse_args()
+
+    print(args.target)
+    print(args.port)
+    print(args.info)
 
 if __name__ == "__main__":
     main()
